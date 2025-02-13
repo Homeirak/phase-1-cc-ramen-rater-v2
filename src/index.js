6ba31e2db17fb13a900c25a694ee6a46f9c48dfc
homeira kalim <homeirakalim@gmail.com>
2025-02-13 01:04:44 -0500

    phase 1 code challenge ramen rater complete

--- src/index.js
+++ src/index.js
@@ -1,29 +1,81 @@
 // index.js
+//DELIVERABLES:
+// See all ramen images in the div with the id of ramen-menu.
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+//FUNCTIONS:
+// main: invokes displayRamens and addSubmitListener after the DOM has fully loaded and start the program logic.
+// displayRamens: requests the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+// callback called handleClick: fires when you click an image from the #ramen-menu to see all the info about that ramen displayed inside the #ramen-detail div (where it says insert comment here and insert rating here).
+// addSubmitListener: attaches a form submit to the Add New Ramen "create" button create a new ramen and add it to the#ramen-menu  div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
 
-const addSubmitListener = () => {
-  // Add code
-}
-
-const displayRamens = () => {
-  // Add code
-};
+let ramenInfo;
+let ramen;
+let ramenMenu;
+let newImg;
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+  document.addEventListener("DOMContentLoaded", () => {
+    displayRamens(ramenInfo);
+    addSubmitListener();
+  });
+};
+main();
 
-main()
+function displayRamens() {
+  fetch("http://localhost:3000/ramens")
+  .then(resp => resp.json())
+  .then(ramenInfo => {
+      ramen = ramenInfo;
+      ramenMenu = document.getElementById("ramen-menu");
+
+      ramen.forEach(ramen => {
+        const img = document.createElement("img");
+        ramenMenu.appendChild(img);
+        img.src = ramen.image;
+
+        img.addEventListener("click", () => handleClick(ramen));
+      })
+
+    console.log(ramenInfo);
+
+  })
+};
+
+const handleClick = (ramen) => {
+  document.querySelector(".detail-image").src =ramen.image;
+  document.querySelector(".name").textContent = ramen.name;
+  document.querySelector(".restaurant").textContent = ramen.restaurant;
+  document.querySelector("#rating-display").textContent = ramen.rating;
+  document.querySelector("#comment-display").textContent = ramen.comment;
+};
+
+function addSubmitListener(){
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const newRamen = {
+      id: ramen.length +1,
+      name: document.querySelector("#new-name").value,
+      restaurant: document.querySelector("#new-restaurant").value,
+      image: document.querySelector("#new-image").value,
+      rating: document.querySelector("#new-rating").value,
+      comment: document.querySelector("#new-comment").value
+    };
+      newImg = document.createElement("img");
+      newImg.src = newRamen.image;
+
+      newImg.addEventListener("click", () => handleClick(newRamen));
+
+      ramenMenu.appendChild(newImg);
+    
+  }
+)};
+
 
 // Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+// export {
+//   displayRamens,
+//   addSubmitListener,
+//   handleClick,
+//   main,
+// };
